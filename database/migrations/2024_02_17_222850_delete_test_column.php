b844b28956d3dc57effac8e978ebeb4c6153aba9
ryowa <kamijyou76@yahoo.co.jp>
2024-03-24 15:09:07 +0900

    gpt先生

--- database/migrations/2024_02_17_222850_delete_test_column.php
+++ database/migrations/2024_02_17_222850_delete_test_column.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-
 return new class extends Migration
 {
     /**
@@ -23,8 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('test');
+            $table->string('test')->after('e-mail');
         });
     }
 };
-
