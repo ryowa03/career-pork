c3e7498626670f9e9fa781349d07892a4669552c
ryowa <kamijyou76@yahoo.co.jp>
2024-03-26 00:58:42 +0900

    RefreshSessionifExpiredの作成

---
+++ app/Http/Middleware/RefreshSessionIfExpired.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Facades\Session;
+
+class RefreshSessionIfExpired extends StartSession
+{
+    public function handle($request, Closure $next)
+    {
+        if (Session::has('lastActivity')) {
+            $lastActivity = Session::get('lastActivity');
+            $sessionLifetime = config('session.lifetime') * 60;
+
+            if (time() - $lastActivity > $sessionLifetime) {
+                // セッションの有効期限が切れた場合はセッションを再生成する
+                $request->session()->regenerate();
+            }
+        }
+
+        // 最終アクティビティを更新
+        Session::put('lastActivity', time());
+
+        return parent::handle($request, $next);
+    }
+}
