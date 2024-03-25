4cbb85b60651567ba966ab2606e0b3eb1f717c35
ryowa <kamijyou76@yahoo.co.jp>
2024-03-26 08:50:49 +0900

    AuthenticatedSessionもコメントアウト

--- app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Session;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -57,5 +58,6 @@ public function create(): View
     }
 
 
+    
 
 }
