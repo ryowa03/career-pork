afccaaf9467a51bb0fd96de8166857b15f179c10
ryowa <kamijyou76@yahoo.co.jp>
2024-03-30 17:18:43 +0900

    dashboardの文言変更

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,7 @@
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900">
                 <a href="{{ route('post.index') }}" class="inline-flex items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-xl text-white uppercase tracking-widest hover:bg-green-700 focus:bg-green-700 active:bg-green-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                    企業情報一覧へ
+                    しごと情報一覧へ
                 </a>
             </div>
         </div>
