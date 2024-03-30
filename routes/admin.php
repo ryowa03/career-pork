d383d115ad5a3c49c8b3ebba2e8e59ffb8cad1fd
ryowa <kamijyou76@yahoo.co.jp>
2024-03-30 12:32:26 +0900

    整理

--- routes/admin.php
+++ routes/admin.php
@@ -11,11 +11,24 @@ use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::middleware('guest:admin')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
+
     Route::post('register', [RegisteredUserController::class, 'store']);
+
+
+
+    // Route::middleware('guest:admin')->group(function () {
+    //     Route::get('admin.register', [RegisteredAdminController::class, 'create'])
+    //                 ->name('admin_register');
+    
+    //     Route::post('admin.register', [RegisteredAdminController::class, 'store']);
+
+
+    
     
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
